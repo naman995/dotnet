119ca508d2b6cf7c9be944401ac50c9d49df2f2c
naman995 <rajputnaman823@gmail.com>
2024-06-07 08:54:38 +0530

    hypotinuse calculator

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+// TO find the hypostinuse of right angle triangle.
+Console.WriteLine("Enter the value of side A");
+double sideA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the value of side B");
+double sideB = Convert.ToDouble(Console.ReadLine());
+
+double h = Math.Sqrt(sideA * sideA + sideB * sideB);
+Console.WriteLine("The hypotenuse of the triangle is " + h.ToString("F2"));
